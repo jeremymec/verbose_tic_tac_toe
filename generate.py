d16470bfada198d6bf916f378c4efbe59a941dea
Jeremy Mechen <jeremymechen1997@gmail.com>
2020-11-23 02:37:07 +1300

    Update

--- generate.py
+++ generate.py
@@ -1,11 +1,13 @@
 from copy import deepcopy
 
 def print_board(board):
-    print("----")
-    print(board[0][0], board[0][1], board[0][2])
-    print(board[1][0], board[1][1], board[1][2])
-    print(board[2][0], board[2][1], board[2][2])
-    print("----")
+    board_string = ""
+    board_string += "----\\n"
+    board_string += (str(board[0][0]) + str(board[0][1]) + str(board[0][2]) + '\\n')
+    board_string += (str(board[1][0]) + str(board[1][1]) + str(board[1][2]) + '\\n')
+    board_string += (str(board[2][0]) + str(board[2][1]) + str(board[2][2]) + '\\n')
+    board_string += "----\\n"
+    return board_string
 
 def next_move(move):
     if (move == 'X'):
@@ -16,29 +18,34 @@ def next_move(move):
 
 board = [[0 for x in range(3)] for y in range(3)]
 
-counter = 0
+input_statement = '{indent}move = input("Enter your move")'
+if_statement = '{indent}if (move == "{y},{x}"):'
+print_board_statement = '{indent}print("{board_string}")'
 
 def it():
     global counter
     counter = 0
     for y in range(3):
         for x in range(3):
-            iterate(deepcopy(board), x, y, 'X', 0)
+            print(input_statement.format(indent=''))
+            print(if_statement.format(indent='', y=y, x=x))
+            iterate(deepcopy(board), x, y, 'X', 1)
 
 
 def iterate(board, move_x, move_y, move_val, depth):
+    # if (depth > 3): return
     board[move_y][move_x] = move_val
-    global counter
-    print_board(board)
-    print(depth)
-    terminal = True
+    print(print_board_statement.format(indent='  ' * depth, board_string=print_board(board)))
+    if (depth > 8): return
+    print(input_statement.format(indent='  ' * depth))
+    print(if_statement.format(indent='  ' * depth, y=move_y, x=move_x))
     for y in range(3):
         for x in range(3):
             if (board[y][x] == 0):
-                terminal = False
+                # terminal = False
                 iterate(deepcopy(board), x, y, next_move(move_val), depth + 1)
-    if terminal:
-        counter = counter + 1
+    # if terminal:
+    #     counter = counter + 1
 
 
 it()
